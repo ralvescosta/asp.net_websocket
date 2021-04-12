26ad0d394fb2aafd8532d6293b4eae584e3ea22b
Rafael Alves <rafael.rac.mg@gmail.com>
2021-04-12 19:53:55 -0300

    fist commit

---
+++ AspNet.Api.Websocket.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AspNet.Api.Websocket.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ AspNet.Api.Websocket.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AspNet.Api.Websocket.Domain
+{
+    public class Class1
+    {
+    }
+}
